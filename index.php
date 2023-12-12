6d95b98af239dfbb6559a18e7a7d760634bf086d
Tolrom <max.le.morlot@gmail.com>
2023-12-12 09:31:20 +0100

    1ers exercices

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    echo '<h2>EXERCICE 1</h2><br>';
+
+    $var = 5;
+    echo $var;
+    echo '<br>';
+    echo gettype($var);
+    echo '<br>';
+    $name = 'maxime';
+    echo $name;
+    echo '<br>';
+    $bool = false;
+    echo gettype($bool);
+    echo '<br>';
+
+    echo '<h2>EXERCICE 2</h2><br>';
+
+    $a = "bon";
+    $b = "jour";
+    $c = 10;
+    echo $a.$b.$c+1;
+    
+    echo '<h2>EXERCICE 3</h2><br>';
+
+    $a = 'Bonjour ';
+    echo '<p>'.$a.'l\'Adrar</p>';
+?>
